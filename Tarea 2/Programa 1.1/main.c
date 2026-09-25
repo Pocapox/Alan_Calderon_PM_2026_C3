5cff502b85875897412c2dcd2b5926eca1a729b9
Alan Calderon <alancalderon690@gmail.com>
2026-09-24 22:53:57 -0400

    Add files via upload

---
+++ Tarea 2/Programa 1.1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Programa 1.1
+El siguiente es el primer programa escrito en lenguaje c. */
+
+void main(void)
+{
+    printf("Mi primer programa en c");}
+
+
